db1d7a754910d81a155ec203f3bf1b01faeab699
k-hench <khench@posteo.net>
2021-02-18 15:53:41 +0100

    init

---
+++ R/demo.R
@@ -0,0 +1,29 @@
+#' Silly Warning
+#'
+#' \code{indicate_concerns} prompts a silly warning to the console
+#'
+#' This function was included for demo purposes.
+#' It is used as a dummy function with some elaborate
+#' documentation.
+#'
+#' @examples
+#' indicate_concerns()
+#'
+#' @export
+indicate_concerns <- function(){
+  ## give silly warning
+  cat("O Dear!")
+}
+
+#' Dummy Table
+#'
+#' \code{demo_table} provides a tiny dummy data set
+#'
+#' This data set was included for demo purposes.
+#'
+#' @examples
+#' demo_table
+#'
+#' @export
+demo_table <- data.frame(x = 1:3,
+                         y = 4:6)
---
+++ README.md
@@ -0,0 +1,13 @@
+# The testpkg Package
+
+This is a small demo of a R package created with {usethis}.
+It makes use of the helper packages {usethis}, {devtools}, {roxygen2} and {pkgdown} to quickly wrap R code into R packages.
+
+To install this package please run this in R:
+
+```
+install.packages("remotes")
+remotes::install_github("k-hench/testpkg")
+```
+
+But honestly --- there is not really a good reason for you to install this ^^
